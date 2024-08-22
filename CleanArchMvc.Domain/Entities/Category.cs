83ff484b6c454cccf3920acb0ee494442e73ccbf
josePC <jose.csousa22@gmail.com>
2024-08-22 18:33:18 -0300

    Cria entidades com dominio rico

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -1,11 +1,35 @@
+using CleanArchMvc.Domain.Validation;
+
 namespace CleanArchMvc.Domain.Entities;
 
-public class Category
+public sealed class Category : EntityBase
 {
-    // um modelo anemico é quando a classe não tem validações, contrutores parametrizado etc. Um exemplo é um dto
-    public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; private set; }
     
-    // Propriedade de navegação
     public ICollection<Product> Products { get; set; }
+
+    public Category(int id, string name)
+    {
+        DomainExceptionValidation.When(id <= 0, "Id must be greater than zero.");
+        Id = id;
+        Validate(name);
+    }
+    
+    public Category(string name)
+    {
+        Validate(name);
+    }
+
+    public void Update(Category category)
+    {
+        Validate(category.Name);
+    }
+    
+    private void Validate(string name)
+    {
+        DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Name cannot be null or empty.");
+        DomainExceptionValidation.When(name.Length < 3, "Name must be at least 3 characters long.");
+        
+        Name = name;
+    }
 }
--- CleanArchMvc.Domain/Entities/Product.cs
+++ CleanArchMvc.Domain/Entities/Product.cs
@@ -1,14 +1,72 @@
+using CleanArchMvc.Domain.Validation;
+
 namespace CleanArchMvc.Domain.Entities;
 
-public class Product
+public sealed class Product : EntityBase
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public decimal Price { get; set; }
-    public int Stock { get; set; }
-    public string Image { get; set; }
-    
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+    public decimal Price { get; private set; }
+    public int Stock { get; private set; }
+    public string Image { get; private set; }
+
     public int CategoryId { get; set; }
     public Category Category { get; set; }
+
+    public Product(string name, string description, decimal price, int stock, string image)
+    {
+        Validate(name, description, price, stock, image);
+   
+        Name = name;
+        Description = description;
+        Price = price;
+        Stock = stock;
+        Image = image;
+    }
+
+    public Product(int id, string name, string description, decimal price, int stock, string image)
+    {
+        DomainExceptionValidation.When(id <= 0, "Invalid Id value");
+        Validate(name, description, price, stock, image);
+        
+        Id = id;
+        Name = name;
+        Description = description;
+        Price = price;
+        Stock = stock;
+        Image = image;
+    }
+
+    public void Update(string name, string description, decimal price, int stock, string image, int categoryId)
+    {
+        Validate(name, description, price, stock, image);
+        
+        Name = name;
+        Description = description;
+        Price = price;
+        Stock = stock;
+        Image = image;
+        CategoryId = categoryId;
+    }
+
+    private void Validate(string name, string description, decimal price, int stock, string image)
+    {
+        DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+            "Invalid product name.");
+
+        DomainExceptionValidation.When(name.Length < 3,
+            "Name must be at least 3 characters long.");
+        
+        DomainExceptionValidation.When(description.Length < 5, 
+            "Invalid product description.");
+        
+        DomainExceptionValidation.When(price < 0, 
+            "Invalid product price.");
+        
+        DomainExceptionValidation.When(stock < 0, 
+            "Invalid stock.");
+        
+        DomainExceptionValidation.When(image.Length > 250, 
+            "Invalid image name, too long, maximum 250 characters.");
+    }
 }
